544a10f981bd3bb2ed40e1e0f6386d8402e50f5a
Greed <kolobrod1994@gmail.com>
2024-08-26 10:31:06 +0300

    Fix useless arg

--- src/database/contestant_table_helper.py
+++ src/database/contestant_table_helper.py
@@ -5,7 +5,7 @@ from database.crud.contestant import add_contestant_to_db
 
 async def create_contestants(db):
     async with db.session_factory.begin() as db_session:
-        await add_contestant_to_db(db_session, db)
+        await add_contestant_to_db(db_session)
 
 
 def main():

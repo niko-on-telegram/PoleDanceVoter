e840ad87c2f07ad50f2d687a9d5b49d05892d9e0
Greed <kolobrod1994@gmail.com>
2024-08-26 11:21:58 +0300

    Add voter mechanism

--- src/bot/handlers/voter_handler.py
+++ src/bot/handlers/voter_handler.py
@@ -6,13 +6,16 @@ from bot.enums import VoterEnum, ContestantEnum
 from bot.callbacks.voter_factory import VoterCallbackFactory
 from bot.callbacks.contestan_factory import ContestantCallbackFactory
 from bot.handlers.contestant_handler import callback_profile
+from database.crud.contestant import inc_vote_to_db, get_contestant_from_db
 
 router = Router()
 
 
 @router.callback_query(VoterCallbackFactory.filter(F.action == VoterEnum.VOTE))
-async def callback_check_answer(callback: types.CallbackQuery, callback_data: VoterCallbackFactory):
-    await callback.message.answer(text=f"VOTE {callback_data.tg_id}")
+async def callback_check_answer(
+    callback: types.CallbackQuery, callback_data: VoterCallbackFactory, db_session: AsyncSession
+):
+    await inc_vote_to_db(callback_data.tg_id, db_session)
     await callback.answer("Спасибо за ваш голос!")
 
 
--- src/database/crud/contestant.py
+++ src/database/crud/contestant.py
@@ -1,6 +1,15 @@
 from sqlalchemy import Result, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from database.models import Contestant
+from sqlalchemy import update
+
+
+async def inc_vote_to_db(tg_id: int, db_session: AsyncSession):
+    query = select(Contestant).filter(Contestant.telegram_id == tg_id)
+    result: Result = await db_session.execute(query)
+    contestant = result.scalar()
+    updates = update(Contestant).where(Contestant.telegram_id == tg_id).values(count_votes=contestant.count_votes + 1)
+    await db_session.execute(updates)
 
 
 async def get_contestant_from_db(tg_id: int, db_session) -> Contestant:

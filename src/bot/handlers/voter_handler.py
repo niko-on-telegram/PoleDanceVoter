40cf3d9c97b103219dd56c9db084081f046129b1
Greed <kolobrod1994@gmail.com>
2024-08-27 10:56:49 +0300

    Add back to contestants list after vote

--- src/bot/handlers/voter_handler.py
+++ src/bot/handlers/voter_handler.py
@@ -5,25 +5,32 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from bot.enums import VoterEnum, ContestantEnum
 from bot.callbacks.voter_factory import VoterCallbackFactory
 from bot.callbacks.contestan_factory import ContestantCallbackFactory
-from bot.handlers.contestant_handler import callback_back as contestant_list, callback_profile
+from bot.handlers.contestant_handler import callback_back as back_to_contestants, callback_profile
 from database.crud.contestant import inc_dec_vote_to_db as inc_dec_contestant_vote
 from database.crud.user import inc_dec_vote_to_db as inc_dec_user_vote, get_user_from_db_by_tg_id
 from database.crud.voter import get_all_voter_ids, add_voter_to_db
+
+from database.models import User
 
 router = Router()
 
 
 @router.callback_query(VoterCallbackFactory.filter(F.action == VoterEnum.VOTE))
 async def callback_vote(callback: types.CallbackQuery, callback_data: VoterCallbackFactory, db_session: AsyncSession):
+    fab = ContestantCallbackFactory(
+        user_id=callback_data.user_id, contestant_id=callback_data.contestant_id, action=ContestantEnum.BACK
+    )
     user = await get_user_from_db_by_tg_id(callback_data.user_id, db_session)
     if user.count_votes >= 3:
         await callback.answer(text="Вы уже проголосовали допустимое количество раз!")
+        await back_to_contestants(callback=callback, callback_data=fab, db_session=db_session, user=user)
         return
 
     voters = await get_all_voter_ids(callback_data.user_id, db_session)
     for voter in voters:
         if voter.contestant_id == callback_data.contestant_id:
             await callback.answer(text="Вы уже голосовали за этого участника!")
+            await back_to_contestants(callback=callback, callback_data=fab, db_session=db_session, user=user)
             return
 
     await inc_dec_contestant_vote(callback_data.contestant_id, db_session)
@@ -32,15 +39,7 @@ async def callback_vote(callback: types.CallbackQuery, callback_data: VoterCallb
         user_id=callback_data.user_id, contestant_id=callback_data.contestant_id, db_session=db_session
     )
     await callback.answer("Спасибо за ваш голос!")
-    #TODO: Переделать функцию чтобы выходить на список участников при любом ветке алгоритма
-    #TODO: Может вынести в base_handler ???
-    await contestant_list(
-        callback=callback,
-        callback_data=ContestantCallbackFactory(
-            user_id=callback_data.user_id, contestant_id=callback_data.contestant_id, action=ContestantEnum.BACK
-        ),
-        db_session=db_session,
-    )
+    await back_to_contestants(callback=callback, callback_data=fab, db_session=db_session, user=user)
 
 
 @router.callback_query(VoterCallbackFactory.filter(F.action == VoterEnum.BACK))

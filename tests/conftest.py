5537b073e2221fb56fd260779f89d9c29e07726d
Greed <kolobrod1994@gmail.com>
2024-08-31 23:59:01 +0300

    add default objects for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,3 +27,30 @@ async def db(pytestconfig):
 @pytest.fixture
 def default_user() -> User:
     return User(telegram_id=361557982, fullname="Greed", count_votes=0)
+
+
+@pytest.fixture
+def default_user_username() -> User:
+    return User(telegram_id=361557982, username="Uff", fullname="Greed", count_votes=0)
+
+
+@pytest.fixture
+def default_user_list() -> list[User]:
+    return [
+        User(telegram_id=361557982, fullname="User1", count_votes=0),
+        User(telegram_id=361557984, fullname="User2", count_votes=1),
+        User(telegram_id=361557985, fullname="User3", count_votes=2),
+        User(telegram_id=361557986, fullname="User4", count_votes=3),
+        User(telegram_id=361557987, fullname="User5", count_votes=4),
+    ]
+
+
+@pytest.fixture
+def default_user_list_username() -> list[User]:
+    return [
+        User(telegram_id=361557982, fullname="User1", count_votes=0),
+        User(telegram_id=361557984, fullname="User2", count_votes=1),
+        User(telegram_id=361557985, fullname="User3", count_votes=2),
+        User(telegram_id=361557986, fullname="User4", count_votes=3),
+        User(telegram_id=361557987, fullname="User5", count_votes=4),
+    ]

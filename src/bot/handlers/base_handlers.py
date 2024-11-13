4e6b1f1b2e095bf25c7d1feaa50ca4096a69d5e5
niko-on-telegram <nikoontelegram@gmail.com>
2024-11-13 17:05:45 +0400

    video handler

--- src/bot/handlers/base_handlers.py
+++ src/bot/handlers/base_handlers.py
@@ -1,4 +1,6 @@
-from aiogram import Router, types
+import logging
+
+from aiogram import Router, types, F
 from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
 
@@ -11,3 +13,12 @@ router = Router()
 async def start_message(message: types.Message, db_session, state: FSMContext) -> None:
     await state.set_state()
     await print_constestant_list(message, db_session)
+
+
+@router.message(F.video)
+async def video_message(message: types.Message) -> None:
+    if message.from_user.id != 99988303:
+        logging.info("Unexpexted video sent")
+        return
+
+    await message.answer(f"{message.video.file_name=} {message.video.file_id=}")

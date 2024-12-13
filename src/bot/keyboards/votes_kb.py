2d16479a58912575e5e3fd7b004c514ff39129cc
niko-on-telegram <nikoontelegram@gmail.com>
2024-12-13 23:54:49 +0200

    final fix

--- src/bot/keyboards/votes_kb.py
+++ src/bot/keyboards/votes_kb.py
@@ -5,15 +5,15 @@ from bot.callbacks.votes_factory import VotesCallbackFactory
 from bot.enums import VotesEnum
 
 
-# def votes_keyboard(contestant_id: int) -> InlineKeyboardMarkup:
-#     kb = InlineKeyboardBuilder()
-#     kb.button(
-#         text="Проголосовать",
-#         callback_data=VotesCallbackFactory(contestant_id=contestant_id, action=VotesEnum.VOTE),
-#     )
-#     kb.button(
-#         text="Назад",
-#         callback_data=VotesCallbackFactory(contestant_id=contestant_id, action=VotesEnum.BACK),
-#     )
-#     kb.adjust(1)
-#     return kb.as_markup()
+def votes_keyboard(contestant_id: int) -> InlineKeyboardMarkup:
+    kb = InlineKeyboardBuilder()
+    kb.button(
+        text="Проголосовать",
+        callback_data=VotesCallbackFactory(contestant_id=contestant_id, action=VotesEnum.VOTE),
+    )
+    kb.button(
+        text="Назад",
+        callback_data=VotesCallbackFactory(contestant_id=contestant_id, action=VotesEnum.BACK),
+    )
+    kb.adjust(1)
+    return kb.as_markup()
